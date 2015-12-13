b114b3ff622004936f7739743dc3accb82076da8
bocharsky-bw <bocharsky.bw@gmail.com>
2015-12-13 22:22:23 +0200

    Add .travis and create and config unit tests directory structure

---
+++ tests/FileNamingResolverTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\FileNamingResolver;
+
+/**
+ * @author Victor Bocharsky <bocharsky.bw@gmail.com>
+ */
+class FileNamingResolverTest extends \PHPUnit_Framework_TestCase
+{
+
+}
