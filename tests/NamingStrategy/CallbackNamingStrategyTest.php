1c75bc92a8078af405cccae3644553e1220db10b
bocharsky-bw <bocharsky.bw@gmail.com>
2016-02-26 23:58:42 +0200

    Cover exception throwing in the CallbackNamingStrategy

--- tests/NamingStrategy/CallbackNamingStrategyTest.php
+++ tests/NamingStrategy/CallbackNamingStrategyTest.php
@@ -10,6 +10,32 @@ use FileNamingResolver\NamingStrategy\CallbackNamingStrategy;
  */
 class CallbackNamingStrategyTest extends \PHPUnit_Framework_TestCase
 {
+    public function testProvideName()
+    {
+        $strategy = new CallbackNamingStrategy(function(FileInfo $srcFileInfo) {
+            return $srcFileInfo
+                ->changePath($srcFileInfo->getPath().'/dir1')
+                ->changeBasename('file1')
+            ;
+        });
+        $srcFileInfo = new FileInfo('/dir/file.ext');
+        $dstFileInfo = $strategy->provideName($srcFileInfo);
+        $this->assertInstanceOf('FileNamingResolver\FileInfo', $dstFileInfo);
+        $this->assertEquals('/dir/dir1/file1.ext', $dstFileInfo->toString());
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testProvideNameException()
+    {
+        $strategy = new CallbackNamingStrategy(function(FileInfo $fileInfo) {
+            return $fileInfo->toString();
+        });
+        $srcFileInfo = new FileInfo(__FILE__);
+        $strategy->provideName($srcFileInfo);
+    }
+
     public function testGetFunc()
     {
         $func = function(FileInfo $fileInfo) {
